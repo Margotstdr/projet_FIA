e885901a670cb4ee0d1cc648d9740c0f06a44f11
armennce <151045937+armennce@users.noreply.github.com>
2025-10-29 12:59:33 +0100

    Update ecuries.c

--- Écuries/ecuries.c
+++ Écuries/ecuries.c
@@ -1,2 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ecuries.h"
+#include "pilotes.h"
+
+void ajouterEcurie(Ecurie **ecuries, int *nbEcuries) {
+        // on augmente la taille du tableau d'une case
+        *ecuries = realloc(*ecuries, (*nbEcuries + 1) * sizeof(Ecurie));
+        if (*ecuries == NULL) {
+            printf("Erreur de réallocation mémoire.\n");
+            exit(1);
+        }
+
+        Ecurie nouvelleEcurie;
+
+    printf("\n--- Ajouter une nouvelle écurie ---\n");
+    printf("Nom de l'écurie : ");
+    scanf(" %[^\n]", nouvelleEcurie.nom);
+
+    printf("Pays : ");
+    scanf(" %[^\n]", nouvelleEcurie.pays);
+
+    printf("Année de création : ");
+    scanf("%d", &nouvelleEcurie.anneeCreation);
+
+    printf("Directeur : ");
+    scanf(" %[^\n]", nouvelleEcurie.directeur);
+
+    nouvelleEcurie.points = 0;
+    nouvelleEcurie.actif = 1;
+
+    // Ajout à la fin du tableau
+    (*ecuries)[*nbEcuries] = nouvelleEcurie;
+    (*nbEcuries)++;
+    
+    printf("Écurie ajoutée avec succès ")
+
+
+}
+
+int majPoints (Pilote p1, Pilote p2){
+
+}
+
+void supprimerEcurie (Ecurie *ecurie){
+
+}
+
+void afficherEcurie (Ecurie *ecurie){
+
+}
